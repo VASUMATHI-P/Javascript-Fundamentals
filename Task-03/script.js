edd7af66f1c70e64e1ad859229bf5f9024dadaee
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2025-04-02 09:53:42 +0530

    Image Gallery Lightbox

---
+++ Task-03/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.image img').forEach(image => {
+  image.addEventListener('click', () => {
+    document.querySelector('.popup-image').style.display = 'block';
+    document.querySelector('.popup-image img').src = image.getAttribute('src');
+  })
+});
+
+document.querySelector('.popup-image span').addEventListener('click', () => {
+  document.querySelector('.popup-image').style.display = 'none';
+})
+    
